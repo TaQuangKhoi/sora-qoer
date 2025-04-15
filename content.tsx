1d677ae3cb3c8b38d3ca6381d8ff6b9fcbf202e3
Tạ Quang Khôi <khoi0941@gmail.com>
2025-04-16 00:28:16 +0700

    Try overlay content.tsx

---
+++ content.tsx
@@ -0,0 +1,26 @@
+import type {
+  PlasmoCSConfig,
+  PlasmoGetOverlayAnchor,
+  PlasmoGetOverlayAnchorList
+} from "plasmo"
+
+export const config: PlasmoCSConfig = {
+  matches: ["https://sora.com/*"]
+}
+
+export const getOverlayAnchor: PlasmoGetOverlayAnchor = async () => {
+  return document.querySelector(`h2`)
+}
+
+const DoQuyen = () => (
+  <span
+    style={{
+      borderRadius: 4,
+      background: "black",
+      padding: 4
+    }}>
+    Đỗ Quyên
+  </span>
+)
+
+export default DoQuyen
